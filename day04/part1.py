a98e444207b67d1893dcdf7ff835c14df9b8d9cd
Simon Wall <moopyglue@gmail.com>
2021-12-04 21:04:39 +0000

    tidy up and prep for tomorrow + getting debugging working for pythone 3.10

--- day04/part1.py
+++ day04/part1.py
@@ -1,53 +1,27 @@
 
-def check_line (l,n):
-    # check a line(l) of number s to see if they are all list of numbers called(n)
-    for a in range(len(l)):
-        if l[a] not in n: return False
-    return True
-        
-def check_card (c,n):
-    # card(c) and numbers called(n)
-    # return if any horizontal line or card(c) is all in numbers called(n)
-    for a in range(0,24,5):
-        if check_line( c[a:a+5],n): return True
-    # return if any vertical line or card(c) is all in numbers called(n)
-    for a in range(5):
-        if check_line( [c[a],c[a+5],c[a+10],c[a+15],c[a+20]], n): return True
-    # no lines macthed
-    return False
+import common
 
-def sum_unmarked (c,n):
-    # sum all the card(c) numbers which have not been called(n)
-    sum=0
-    for a in range(len(c)):
-        if c[a] not in n: sum+=int(c[a])
-    return sum
-
-# read in the data file
-with open('rawdata') as inputfile:
-    data=inputfile.readlines()
-
-# allocate data read in from file
-# into list of bingocalls and an array of arrays for cards 
-bingocalls = data[0].strip().split(",") 
-cards=[]
-for x in range(1,len(data),6):
-    # join then split each 6 lines to make cards each as a single array of 25 values
-    cards.append(" ".join(data[x:x+6]).split() )
+file="rawdata"
+bingocalls = common.load_bingo_calls(file)
+cards      = common.load_cards(file)
 
 # for each bingo call ...
 for a in range(len(bingocalls)):
+
+    called=bingocalls[0:a+1]
     # for each card ...
     for b in range(len(cards)):
+
         # check each card against numbers called so far
-        if check_card(cards[b],bingocalls[0:a+1]):
-            # found matching card caluclate reulst and print
-            print("card=%d, sum_unmarked=%d, lastcalled=%s" 
-                  % (b,sum_unmarked(cards[b],bingocalls[0:a+1]),bingocalls[a] ))
-            print("result=%d" % 
-                  (sum_unmarked(cards[b],bingocalls[0:a+1])*int(bingocalls[a])))
-            exit(0)
-        
-    
-# print(cards)
-# print(bingocalls)
+        if common.check_card(cards[b],called):
+            # found first matching card and brealk from look
+            first_card=cards[b].copy()
+            break
+
+    if 'first_card' in locals(): break
+
+# results printing
+sm=common.sum_unmarked(first_card,called)
+print("first_card",first_card)
+print("card=%d, sum_unmarked=%d, lastcalled=%s" % (b,sm,bingocalls[a] ))
+print("result=%d" % (sm*int(bingocalls[a])))
