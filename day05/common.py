b87facffa6413869cc0ad7395864f65466e67266
Simon Wall <moopyglue@gmail.com>
2021-12-05 08:38:43 +0000

    fixed comments

--- day05/common.py
+++ day05/common.py
@@ -2,18 +2,19 @@
 import re
 
 def load_lines (file):
-    # load data file into memory
     with open(file) as inputfile:
-        # read in the list of bingo balls only(first line) - list of strings
+        # convert each line into a list of 4 integers
         lines=[]
         for line in inputfile.readlines():
             linelist=[]
+            # split line on any 1+ non-numical(0-9) chars, 
             for val in re.split("[^0-9]+",line.strip()):
                 linelist.append(int(val))
             lines.append(linelist)
     return lines            
 
 def plot_vent(dic,x,y):
+    # +1 a dictionary element
     s = str(x)+","+str(y)
     if s in dic:
         dic[s] += 1
@@ -21,6 +22,7 @@ def plot_vent(dic,x,y):
         dic[s] = 1
 
 def sign(n):
+    # missing maths function created here
     if n>0: return 1
     if n<0: return -1
     return 0
