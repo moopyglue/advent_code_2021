11d158d37175c9406cf09686ec5af3e6abd888d8
Simon Wall <moopyglue@gmail.com>
2021-12-07 06:06:50 +0000

    day 7

---
+++ day07/part2.py
@@ -0,0 +1,30 @@
+
+print("_"*100)
+
+# read in the list of horizontal positions
+# and calculate the minimum and maximum
+hozpos=[] 
+with open("rawdata") as inputfile:
+    for n in inputfile.readline().strip().split(","):
+        hozpos.append(int(n))
+hozpos.sort()
+minpos=hozpos[0]
+maxpos=hozpos[len(hozpos)-1]
+
+costs=[]
+# loop through each possible final position and calculate
+# a cost for moving all crabs to that position 
+for x in range(minpos,maxpos):
+    fuelcost=0
+    for crabsub in hozpos:
+        # calculate number of moves
+        moves = abs(crabsub-x)
+        # calculate fuel cost for that number of moves
+        fuelcost += int((((moves*moves)+moves))/2)
+    costs.append(fuelcost)
+
+# sort the list of costs to find the cheapest
+costs.sort()
+print("result =",costs[0])
+
+print("¬"*100)
